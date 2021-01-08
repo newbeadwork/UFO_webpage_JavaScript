64f7efbc1d298f195890a150c0dab4818b70994b
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-07 18:52:07 -0800

    Filling a table code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(sightingUFO) {
+    row = tbody.append("tr");
+    Object.entries(sightingUFO).forEach(function([key, value]) {
+        row.append("td").text(value);
+    });
+});
