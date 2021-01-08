2d4b8f6766828ca6aff6c03e52575b1f90939137
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-08 15:23:36 -0800

    Finished working solution with add. function for part 2 HW

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -25,67 +25,44 @@ form.on("submit", checkIng);
 // Complete the event handler function for the form
 function checkIng() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElementDate = d3.select("#datetime");
-  var inputElementCity = d3.select("#city");
-  var inputElementState = d3.select("#state");
-  var inputElementCountry = d3.select("#country");
-  var inputElementShape = d3.select("#shape");
+        // Prevent the page from refreshing
+        d3.event.preventDefault();
+        
+        // Select the input element and get the raw HTML node
+        var inputElementDate = d3.select("#datetime");
+        var inputElementCity = d3.select("#city");
+        var inputElementState = d3.select("#state");
+        var inputElementCountry = d3.select("#country");
+        var inputElementShape = d3.select("#shape");
 
-  // Get the value property of the input element
-  var inputDate = inputElementDate.property("value");
-  var inputCity = inputElementCity.property("value");
-  var inputState = inputElementState.property("value");
-  var inputCountry = inputElementCountry.property("value");
-  var inputShape = inputElementShape.property("value");
-  
-  var filteredData = [];
-  
-  if (inputDate != "") {
-    tbody.html("");
-    filteredData = tableData.filter(sighting => sighting.datetime === inputDate);
-    filteredData.forEach(appendTable); 
-    console.log(1)
-  }
-  else {
-    
-    //existing table should be cleaned (that applies also in cases if
-    //there was not empty user`s input before without refreshing the web-page)
-    tbody.html("");
-    
-    //full data appends to the html table
-    filteredData = tableData; 
-    filteredData.forEach(appendTable);
-   }
+        // Get the value property of the input element
+        var inputDate = inputElementDate.property("value");
+        var inputCity = inputElementCity.property("value");
+        var inputState = inputElementState.property("value");
+        var inputCountry = inputElementCountry.property("value");
+        var inputShape = inputElementShape.property("value");
+        
+        
+        var filteredData = tableData;
+        
+        if (inputDate === "" && inputCity === "" && inputState === "" && inputCountry === "" && inputShape === "") {
+                    tbody.html(""); 
+                    tableData.forEach(appendTable);
+        }
+        
+        function conditionalCheck(input, item) {
+                if (input != "") {
+                    tbody.html(""); 
+                    filteredData = filteredData.filter(sighting => sighting[item] === input);
+                    filteredData.forEach(appendTable);
+                    console.log(item)
+                }
+        }
+        conditionalCheck(inputDate, "datetime");
+        conditionalCheck(inputCity, "city");
+        conditionalCheck(inputState, "state");
+        conditionalCheck(inputCountry, "country");
+        conditionalCheck(inputShape, "shape");
 
-  if (inputCity != "") {
-      tbody.html(""); 
-      filteredData = filteredData.filter(sighting => sighting.city === inputCity);
-      filteredData.forEach(appendTable);
-      console.log(inputCity);
-  }
-  
-  if (inputState != "") {
-    tbody.html("");
-    filteredData = filteredData.filter(sighting => sighting.state === inputState);
-    filteredData.forEach(appendTable);
-    console.log(inputState)
-  }
-  
-  if (inputCountry != "") {
-    tbody.html("");
-    filteredData = filteredData.filter(sighting => sighting.country === inputCountry);
-    filteredData.forEach(appendTable);
-  }
-  
-  
-  if (inputShape != "") {
-    tbody.html("");
-    filteredData = filteredData.filter(sighting => sighting.shape === inputShape);
-    filteredData.forEach(appendTable);
-  }
-  
 }
+        
