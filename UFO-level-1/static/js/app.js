bc7b8dee3f39648070d5527bd754ab1859333e71
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-07 19:09:40 -0800

    Adding starting code for filtring the data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,33 @@ tableData.forEach(function(sightingUFO) {
         row.append("td").text(value);
     });
 });
+
+
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select(".form-group");
+
+// Create event handlers 
+button.on("click", checkIng);
+form.on("submit", checkIng);
+
+// Complete the event handler function for the form
+function checkIng() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+}
+ 
+
+  //var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  //console.log(filteredData);
