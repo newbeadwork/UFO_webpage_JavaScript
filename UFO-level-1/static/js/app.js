7bb6b8b2131a76c896e1b692e8c0398996065b94
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-08 17:01:32 -0800

    Changed images and some styling html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -25,39 +25,41 @@ var form = d3.select(".form-group");
 button.on("click", checkIng);
 form.on("submit", checkIng);
 
-//creating the event handler function for the form
+//creating the event handler function for the button and form
 function checkIng() {
 
-  //preventing the page from refreshing
-  d3.event.preventDefault();
-  
-  //variable for an input element 
-  var inputElement = d3.select("#datetime");
+    //preventing the page from refreshing
+    d3.event.preventDefault();
+    
+    //variable for an input element in html code 
+    var inputElement = d3.select("#datetime");
 
-  //variable for user`s value property of the input element
-  var inputDate = inputElement.property("value");
-  
-  //condition for handling filled/empty user`s input
-  //if user`s input has a value than filtering appears
-  if (inputDate != "") {
+    //variable for user`s input
+    var inputDate = inputElement.property("value");
     
-    //existing table should be cleaned
-    tbody.html("");
+    //condition for handling filled/empty user`s input
     
-    //data from data.js should be filtered on user`s input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputDate);
+    //if user`s input has a value than filtering appears
+    if (inputDate != "") {
+        
+        //existing table should be cleaned
+        tbody.html("");
+        
+        //data from data.js should be filtered on user`s input value
+        var filteredData = tableData.filter(sighting => sighting.datetime === inputDate);
+        
+        //filtered data should be appended to the html table
+        filteredData.forEach(appendTable);
+    }
     
-    //filtered data should be appended to the html table
-    filteredData.forEach(appendTable);
-   }
-  //in other cases (user`s input is empty) return to initial state accures
-  else {
-    
-    //existing table should be cleaned (that applies also in cases if
-    //there was not empty user`s input before without refreshing the web-page)
-    tbody.html("");
-    
-    //full data appends to the html table
-    tableData.forEach(appendTable);  
-   }
+    //in other cases (user`s input is empty) return to initial state accures
+    else {
+        
+        //existing table should be cleaned (that applies also in cases if
+        //there was not empty user`s input before without refreshing the web-page)
+        tbody.html("");
+        
+        //full data appends to the html table
+        tableData.forEach(appendTable);  
+    }
 }
